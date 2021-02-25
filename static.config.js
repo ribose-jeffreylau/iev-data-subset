dfcdc2a36add19e5387a27d03b174051c75bd8a7
Anton Strogonoff <anton@strogonoff.name>
2021-02-25 21:42:38 +0900

    Update static.config.js

--- static.config.js
+++ static.config.js
@@ -17,7 +17,7 @@ export default {
       '@riboseinc/react-static-plugin-paneron-registry',
       {
         datasetSourcePath: DATASET_ROOT,
-        iconURL: 'https://open.ribose.com/assets/favicon-192x192.png',
+        iconURL: 'http://www.electropedia.org/img/logo.jpg',
         urlPrefix: '',
         itemClassConfiguration,
         subregisters,
